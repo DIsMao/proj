6dabda8b3f81c8257420346e0e15d889a30777e6
DIsMao <74967959+DIsMao@users.noreply.github.com>
2025-05-16 19:08:23 +0300

    .

--- js/main.js
+++ js/main.js
@@ -1,35 +1,3 @@
-$(document).ready(function(){
-
-
-
-    $(".scroll").css("width", $(".navItem").width())
-    $(".navItem").on("click",function (param) { 
-        $(".scroll").css("left", $(".navItem").width() * $(this).attr("data-id") + 1 + "px")
-        $(".commentsList").trigger("to.owl.carousel", [$(this).attr("data-id"), 500]) 
-        let num = $(this).attr("data-id")
-        setTimeout(function (param) {
-        $(".commentSlideG").addClass("hidden")
-        $(".commentSlide-" + num).removeClass("hidden");
-
-          },400)
-
-    })
-
-        $('.commentsList').owlCarousel({
-            autoWidth:true,
-            autoHeightClass: 'blogCard-height',
-            autoHeight:true,
-           slideSpeed : 500,
-        //    autoplay:true,
-           items : 2,
-           itemsMobile : true,
-           loop:true,
-           onDragged: callback,
-
-
-        });
-
-})
 function callback(event) {
     // Provided by the core
     var element   = event.target;         // DOM element, in this example .owl-carousel
@@ -47,3 +15,87 @@ function callback(event) {
 
     $(".scroll").css("left", $(".navItem").width() * page + "px")
 }
+
+$(document).ready(function(){
+
+
+
+    $(".scroll").css("width", $(".navItem").width())
+    $(".navItem").on("click",function (param) { 
+        $(".scroll").css("left", $(".navItem").width() * $(this).attr("data-id") + 1 + "px")
+        $(".commentsList").trigger("to.owl.carousel", [$(this).attr("data-id"), 500]) 
+        let num = $(this).attr("data-id")
+        setTimeout(function (param) {
+        $(".commentSlideG").addClass("hidden")
+        $(".commentSlide-" + num).removeClass("hidden");
+
+          },400)
+
+    })
+
+    $('.commentsList').owlCarousel({
+        autoWidth:true,
+        autoHeightClass: 'blogCard-height',
+        autoHeight:true,
+        slideSpeed : 500,
+    //    autoplay:true,
+        items : 2,
+        itemsMobile : true, 
+        loop:true,
+        onDragged: callback,
+
+
+    });
+    $(".openMenuBtn").click(function (e) { 
+     
+      $(".mainMenuPopUp").removeClass("hidden")
+    });
+
+    $(".closeMMenuBtn").on("click", function (e) { 
+      
+      $(".mainMenuPopUp").addClass("hidden")
+      
+    });
+    $(".dateCard").on("click", function (e) { 
+      
+      $(".dateCard").removeClass("active")
+      $(this).addClass("active")
+    });
+    $(".inputList .arrow").on("click", function (e) { 
+      
+      $(this).parent().children(".hiddenList").addClass("active")
+      
+    });
+    $(".hiddenList .option").on("click", function (e) { 
+        
+      $(this).parent().parent().children("p").text($(this).text())
+      $(".hiddenList").removeClass("active")
+      
+    });
+
+
+    $( ".accordionCon" ).each(function( index ) {
+        console.log( index + ": " + $( this ).height() );
+        $( this ).attr("data-height", $( this ).height())
+        
+    });
+    $( ".openAccBtn" ).on("click",function( index ) {
+        let accordionCon = $( this ).parent().next().children(".accordionCon")
+        if($( this ).hasClass("active")){
+            $( this ).removeClass("active")
+            $( this ).parent().parent().removeClass("active")
+            $( this ).parent().next().css("height", "0px")
+        }else{
+            $( this ).addClass("active")
+            $( this ).parent().parent().addClass("active")
+            
+        let h =  accordionCon.height() 
+            $( this ).parent().next().css("height", h + "px")
+        }
+
+
+    });
+    
+})
+
+
